06ce414b5cfaae2feb77769da8087e493709230a
Samir Tapiero <samir.tapiero@rootstrap.com>
2025-03-27 17:45:36 -0500

    Add contact and footer sections, add new icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -8,5 +9,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [icon()],
+  adapter: vercel(),
 });
